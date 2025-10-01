ece7dfef4453e9dd88cd1d9ffea6762ee131256c
AmanSinghThakur01 <thakuramansinghh01@gmail.com>
2025-10-01 23:20:55 +0530

    first commit

---
+++ lib/Screens/Products_showed.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class ProductsShowed extends StatefulWidget {
+  const ProductsShowed({super.key});
+
+  @override
+  State<ProductsShowed> createState() => _ProductsShowedState();
+}
+
+class _ProductsShowedState extends State<ProductsShowed> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
